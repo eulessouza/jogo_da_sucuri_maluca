983ec3bacf56e63b826d836e6251f300a6ec3b24
queixo <necrogamer01@gmail.com>
2022-06-06 12:17:19 -0300

    cria cobrinha

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+snake[0] =  {
+    x: 8 * box,
+    y: 8 * box
+}
 
-function criaBG()   {
+function objBG()    {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-criaBG();
+function objSnake() {
+    for(i=0; i < snake.length; i++) {
+        context.fillStyle = "black";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+objBG();
+objSnake();
